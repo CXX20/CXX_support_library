f2a351cbb18826bca2aa88fdd87a547dfc8f48ac
CXX20 <59842069+CXX20@users.noreply.github.com>
2021-08-14 14:16:21 +0300

    removed `Type`; moved `Value`, `value_v`, `type_v` from utility.h to type_traits.h

--- include/sup/type_traits.h
+++ include/sup/type_traits.h
@@ -8,6 +8,11 @@ template<typename T, typename U>
 concept Fwd = std::constructible_from<U, T&&> &&
 	std::same_as<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 
+template<typename T> std::type_identity_t<T> constexpr type_v;
+
+template<auto v> using Value = std::integral_constant<decltype(v), v>;
+template<auto v> Value<v> constexpr value_v;
+
 template<typename T, template<typename...> typename C>
 class [[deprecated("use C++20 `requires` instead")]] Detect {
 	static constexpr auto test(...) { return std::false_type{}; }
--- include/sup/utility.h
+++ include/sup/utility.h
@@ -11,11 +11,6 @@ requires requires { SUP_FWD(t) == SUP_FWD(u); } {
 	return !(SUP_FWD(t) == SUP_FWD(u));
 } // TODO remove explicit `template`
 
-template<typename T> std::type_identity_t<T> constexpr type_v;
-
-template<auto v> using Value = std::integral_constant<decltype(v), v>;
-template<auto v> Value<v> constexpr value_v;
-
 template<typename... Fs> struct Overload: public Fs... {
 	using Fs::operator()...;
 };
@@ -27,9 +22,4 @@ public:
 	~Defer() noexcept(noexcept(action())) { action(); }
 };
 template<typename F> Defer(F&&) -> Defer<F>;
-
-template<typename T>
-struct [[deprecated("use C++20 `std::type_identity_t` instead")]] Type {
-	using type = T;
-};
 } // namespace sup
